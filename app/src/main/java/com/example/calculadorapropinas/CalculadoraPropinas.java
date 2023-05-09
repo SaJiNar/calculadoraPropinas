52e84fe12ea55b03d52b27c96abe922a809364fa
SaJiNar <sarajnares@gmail.com>
2023-05-09 20:02:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculadorapropinas/CalculadoraPropinas.java
@@ -0,0 +1,17 @@
+package com.example.calculadorapropinas;
+
+public class CalculadoraPropinas {
+    private double montoCuenta;
+    private double porcentajePropina;
+
+    public void setMontoCuenta(double montoCuenta) {
+        this.montoCuenta = montoCuenta;
+    }
+    public void setPorcentajePropina(double porcentajePropina) {
+        this.porcentajePropina = porcentajePropina;
+    }
+    public double calcularTotalPagar() {
+        double propina = montoCuenta * porcentajePropina / 100;
+        return montoCuenta + propina;
+    }
+}
---
+++ app/src/main/java/com/example/calculadorapropinas/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.calculadorapropinas;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etMontoCuenta;
+    private Spinner spPorcentajePropina;
+    private TextView tvTotalPagar;
+    private CalculadoraPropinas calculadoraPropinas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etMontoCuenta = findViewById(R.id.et_monto_cuenta);
+        spPorcentajePropina = findViewById(R.id.sp_porcentaje_propina);
+        tvTotalPagar = findViewById(R.id.tv_total_pagar);
+        Button btnCalcularPropina = findViewById(R.id.btn_calcular_propina);
+
+        calculadoraPropinas = new CalculadoraPropinas();
+
+        btnCalcularPropina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double montoCuenta = Double.parseDouble(etMontoCuenta.getText().toString());
+                double porcentajePropina = Double.parseDouble(spPorcentajePropina.getSelectedItem().toString());
+                Double.parseDouble(spPorcentajePropina.getSelectedItem().toString());
+
+                calculadoraPropinas.setMontoCuenta(montoCuenta);
+                calculadoraPropinas.setPorcentajePropina(porcentajePropina);
+
+                double totalPagar = calculadoraPropinas.calcularTotalPagar();
+                tvTotalPagar.setText(String.format(Locale.getDefault(),"Total a pagar: %.2f", totalPagar));
+            }
+        });
+    }
+}
